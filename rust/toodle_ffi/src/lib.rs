cfc9913accc9ac30a3b342b1d143749a02d4d6fa
Emily Toop <etoop@mozilla.com>
2018-01-22 13:04:31 +0000

    address review comments - relies on https://github.com/mozilla/mentat/pull/529 to land first

--- rust/toodle_ffi/src/lib.rs
+++ rust/toodle_ffi/src/lib.rs
@@ -1,13 +1,23 @@
+// Copyright 2016 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 extern crate ffi_utils;
 extern crate libc;
-extern crate mentat_core;
+extern crate mentat;
 extern crate time;
 extern crate toodle;
 
 mod ctypes;
 
 use libc::{ c_int, size_t, time_t };
-use mentat_core::Uuid;
+use mentat::Uuid;
 use std::ffi::CString;
 use std::os::raw::{
     c_char,
